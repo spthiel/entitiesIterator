1f03843b10e141275ebf05c222c78846d003303c
Unknown <steppi.thiel@googlemail.com>
2018-04-15 01:16:26 +0200

    gitignore I hate you

---
+++ build/sources/main/java/me/spthiel/entities/ModuleInfo.java
@@ -0,0 +1,18 @@
+package me.spthiel.entities;
+
+/**
+ * Shared information for this module
+ */
+public abstract class ModuleInfo {
+    
+    /**
+     * Target API version
+     */
+    public static final int API_VERSION = 24;
+
+    /**
+     * Private ctor, no instances 
+     */
+    private ModuleInfo() {}
+
+}
